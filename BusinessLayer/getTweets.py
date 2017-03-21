1eb33258dfa8c30abd564f0a9752c2cfe63b711a
Harshit Shrivastava <harshit.shrivastava14@gmail.com>
2017-03-21 00:31:19 -0400

    Initial attempt at creating train data

--- BusinessLayer/getTweets.py
+++ BusinessLayer/getTweets.py
@@ -1,6 +1,10 @@
+from __future__ import unicode_literals
 import tweepy
 import os
 import glob
+import random
+import string
+import shutil
 
 #credentials for the app
 API_KEY = "kjhjED58EIPLCB0jTJr9KUCjW"
@@ -25,18 +29,23 @@ def userTweets(user):
 #input: hashtag
 #returns: list object containing tweets
 def hashtagTweets(hashtag):
+    print hashtag
     tweetStore = []
     c = tweepy.Cursor(api.search, q=hashtag, lang="en")
     for status in c.items(500):
         tweetStore.append(status.text)
-    print tweetStore
     return tweetStore
 
 #function to get tweets containing a keyword
 #input: keyword
 #returns: list object containing tweets
 def keywordTweets(keyword):
-    return []
+    print keyword
+    tweetStore = []
+    c = tweepy.Cursor(api.search, q=keyword, lang="en")
+    for status in c.items(500):
+        tweetStore.append(status.text)
+    return tweetStore
 
 #function to create folders for a topic for test and train data
 #input: none
@@ -56,6 +65,9 @@ def createFolders():
 def readTopic(file):
     directory = 'C:/SP17/SMM/Project/Data/Topics/'
     with open(directory + file, 'r') as f:
+        directory = 'C:/SP17/SMM/Project/Data/train/'
+        tweetDir = directory + os.path.splitext(file)[0]
+        clearRepository(tweetDir)
         lines = f.readlines()
         for line in lines:
             if line.startswith('@'):
@@ -63,17 +75,40 @@ def readTopic(file):
                 createTweetData(userTweets(line[1:]), file)
             elif line.startswith('#'):
                 #line represents a hashtag, pass to the function hashTagTweets, and then to createTweetData
-                createTweetData(hashtagTweets(line[1:]), file)
+                createTweetData(hashtagTweets(line), file)
             else:
-                if line.startswith('@'):
                 # line represents a user, pass to the function userTweets, and then to createTweetData
-                    createTweetData(keywordTweets(line), file)
+                createTweetData(keywordTweets(line), file)
+    return
+
+#function to create random file name for each tweet
+#input: length of the file name
+#returns: a random word for file name
+def randomWord(length):
+    return ''.join(random.choice(string.lowercase) for i in range(length))
+
+#function to clear the repository before
+#input: length of the file name
+#returns: a random word for file name
+def clearRepository(repository):
+    for root, dirs, files in os.walk(repository):
+        for f in files:
+            os.unlink(os.path.join(root, f))
+        for d in dirs:
+            shutil.rmtree(os.path.join(root, d))
     return
 
 #function to create individual file for each tweet
 #input: list of tweets for a topic, topic
 #returns: none
 def createTweetData(tweetList, file):
+    directory = 'C:/SP17/SMM/Project/Data/train/'
+    tweetDir = directory + os.path.splitext(file)[0]
+    for tweet in tweetList:
+        tweetFile = tweetDir + '/' + randomWord(10)
+        f = open(tweetFile + '.txt','w+')
+        f.write(tweet.encode('utf-8'))
+        f.close()
     return
 
 #function to parse over the topics
@@ -84,17 +119,6 @@ def readAllTopics(topicsFolder):
     for file in glob.glob('*.txt'):
         readTopic(file)
     return
-
-# def readFile(topic):
-#     tweetBag = []
-#
-#     word = ''
-#     if word.startswith('#'):
-#         tweetBag.append(hashtagTweets(word))
-#     elif word.startswith('@'):
-#         tweetBag.append(userTweets(word))
-#     else:
-#         tweetBag.append(keywordTweets(word))
 
 #function to pull tweets for a topic for both test and train
 #input: topic
